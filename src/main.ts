3444c91d443c097507644f2c02c9428c1bb51f48
josuerx12 <josueaze12@gmail.com>
2025-06-29 21:54:24 -0300

    :hammer:refact: Adicionado tratativa de erro aprimorada.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { UnprocessableEntityException, ValidationPipe } from '@nestjs/common';
 import { ValidationExceptionFilter } from './@shared/application/filters/validation-exception.filter';
 import { InvalidPhoneExceptionFilter } from './@shared/application/filters/invalid-phone-exception.filter';
 import { InvalidCpfExceptionFilter } from './@shared/application/filters/invalid-cpf-exception.filter';
 import { InvalidCnpjExceptionFilter } from './@shared/application/filters/invalid-cnpj-exception.filter copy';
-import { useContainer } from 'class-validator';
+import { useContainer, ValidationError } from 'class-validator';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
@@ -13,14 +13,50 @@ async function bootstrap() {
 
   app.useGlobalPipes(
     new ValidationPipe({
+      errorHttpStatusCode: 422,
       transform: true,
-      whitelist: true,
-      forbidNonWhitelisted: true,
+      stopAtFirstError: true,
+      exceptionFactory: (errors: ValidationError[]) => {
+        const recursiveReduce = (accumulator: object, error: any) => {
+          if (error.children && error.children.length > 0) {
+            return error.children.reduce(recursiveReduce, accumulator);
+          }
+
+          if (error.property in accumulator) {
+            if (
+              error.constraints &&
+              Object.keys(error.constraints).length > 0
+            ) {
+              accumulator[error.property].push(
+                error.constraints[Object.keys(error.constraints)[0]],
+              );
+            }
+          } else {
+            if (
+              error.constraints &&
+              Object.keys(error.constraints).length > 0
+            ) {
+              accumulator[error.property] = [
+                error.constraints[Object.keys(error.constraints)[0]],
+              ];
+            }
+          }
+
+          return accumulator;
+        };
+
+        return new UnprocessableEntityException({
+          statusCode: 422,
+          type: 'ValidationError',
+          message: 'Invalid request body content',
+          error: errors.reduce(recursiveReduce, {}),
+        });
+      },
+      validateCustomDecorators: true,
     }),
   );
 
   app.useGlobalFilters(
-    new ValidationExceptionFilter(),
     new InvalidPhoneExceptionFilter(),
     new InvalidCpfExceptionFilter(),
     new InvalidCnpjExceptionFilter(),
